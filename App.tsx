34c1e8695443d6438d94387e876ed9c3eb79bb63
AdolfoAugustoR <adolfoaugustor@gmail.com>
2021-04-21 16:11:28 -0300

    finalizando primeira aula, botão com efeito para ocultar.

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React, {Fragment} from 'react';
+import { Welcome } from './src/pages/Welcome';
+
+export default function App(){
+  return (
+    <Welcome/>
+  ) 
+}
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import {SafeAreaView, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
+
+import weteringImg from '../assets/watering.png';
+import colors from '../styles/colors';
+import {Button} from '../components/Button';
+
+export function Welcome(){
+   const [visible, setVisible] = useState(false);
+
+   function handleVisibility(){
+      setVisible(true);
+   }
+
+   return (
+      <SafeAreaView style={styles.container}>
+         <Text style={styles.title}>
+            Gerencie {'\n'}
+            suas plantas {'\n'}
+            de forma fácil
+         </Text>
+         {
+            visible &&
+            <Image source={weteringImg} style={styles.image} />
+         }
+         
+         <Text style={styles.subtitle}>
+            Não esqueça maias de regar suas plantas. 
+            Nós cuidamos de lembrar você sempre que precisar.
+         </Text>
+         <Button title="image" onPress={handleVisibility}/>
+      </SafeAreaView>
+   )
+}
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      alignItems:'center',
+      justifyContent: 'space-between'
+   },
+   title: {
+      fontSize:32,
+      fontWeight:'bold',
+      textAlign: 'center',
+      color:colors.heading,
+      marginTop:50
+   },
+   subtitle: {
+      textAlign: 'center',
+      fontSize:18,
+      paddingHorizontal:20,
+      color:colors.heading
+   },
+   button: {
+      backgroundColor: colors.green,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 16,
+      marginBottom: 30,
+      height: 55,
+      paddingHorizontal:10
+   },
+   image: {
+      width:292,
+      height:284
+   },
+   buttonText: {
+      color:colors.white,
+      fontSize:24
+   }
+});
